c5bf50ecc31610d8dd370578a72b5ea47d9ca818
aleynagulkazdal <135385893+aleynagul@users.noreply.github.com>
2025-12-22 00:51:59 +0300

    Add JWT login endpoint

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,31 @@
 import { Router} from "express";
+import jwt from 'jsonwebtoken';
 
 const router = Router();
+
+//kullanıcı adı
+const USER = {
+    username: 'aley',
+    password: '1234'
+};
+
+const JWT_SECRET = 'supersecretkey';
 
 //test login endpoint
 
 router.post('/login',(req,res) => {
-    res.json ({
-        message : 'login endpoint çalışıyor.'
-    });
+    const {username,password}=req.body;
+
+    if(username !== USER.username || password !==USER.password){
+        return res.status(401).json({message: 'geçersiz kullanıcı bilgileri'});
+    }
+    const token = jwt.sign (
+        {username},
+        JWT_SECRET,
+        {expiresIn: '1h'}
+    );
+    
+    res.json ({ token });
 });
 
 export default router;
